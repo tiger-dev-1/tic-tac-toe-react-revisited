86fbe3cc577447f059daecd1a304594e9d1f97ec
JP <jota.tiger.dev@gmail.com>
2024-11-08 20:42:08 -0300

    Although we have 9 squares now, they're disarranged

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ export default function Square() {
     <>
       <button className="square">X</button>
       <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
     </>
   );
 }
